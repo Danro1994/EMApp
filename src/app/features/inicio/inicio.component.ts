20a96cc0be4772c220594f766dd919f598821389
unknown <daniel.rodriguez1@unitec.edu>
2020-09-13 21:02:01 -0600

    Frontend

--- src/app/features/inicio/inicio.component.ts
+++ src/app/features/inicio/inicio.component.ts
@@ -6,7 +6,7 @@ import {Router} from '@angular/router';
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+  styleUrls: ['./inicio.component.css'],
 })
 export class InicioComponent implements OnInit {
   usuarios:Usuario[];
